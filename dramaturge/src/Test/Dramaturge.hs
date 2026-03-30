6c9c911459e027bf7286117f610864f7f53a7eff
ners <ners@gmx.ch>
2026-03-30 14:13:58 +0200

    dramaturge: expose helper functions

--- dramaturge/src/Test/Dramaturge.hs
+++ dramaturge/src/Test/Dramaturge.hs
@@ -1,63 +1,175 @@
-{-# LANGUAGE RoleAnnotations #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Test.Dramaturge
     ( module Test.Dramaturge
+    , module Test.Dramaturge.Log
+    , module Test.Dramaturge.Config
+    , module Effectful.Concurrent
+    , module Effectful.Concurrent.Async
+    , module Effectful.Exception
+    , module Effectful.FileSystem
+    , module Effectful.FileSystem.IO.ByteString
     , module Effectful.Marionette
+    , module Effectful.Process.Typed
+    , module Effectful.Timeout
+    , module GHC.Stack
     )
 where
 
-import Control.Exception (AssertionFailed (AssertionFailed))
-import Control.Monad ((>=>))
+import Control.Monad ((<=<))
 import Control.Monad.Catch (MonadThrow (throwM))
+import Control.Monad.Extra (untilJustM)
 import Data.Aeson (ToJSON (toJSON))
-import Data.Functor.Identity (Identity (..))
-import Data.Maybe (isJust)
-import Effectful (Dispatch (Static), DispatchOf, Eff, Effect, IOE, (:>))
-import Effectful.Dispatch.Static
-    ( SideEffects (WithSideEffects)
-    , StaticRep
-    , evalStaticRep
-    )
-import Effectful.Log (LogLevel)
+import Data.String (fromString)
+import Data.Text (Text)
+import Data.Text.Encoding qualified as Text
+import Data.These (These (..))
+import Effectful
+import Effectful.Concurrent (Concurrent, runConcurrent, threadDelay)
+import Effectful.Concurrent.Async (concurrently)
+import Effectful.Exception (Exception (..), SomeException, catch, try)
+import Effectful.FileSystem (FileSystem, runFileSystem)
+import Effectful.FileSystem.IO.ByteString (writeFile)
 import Effectful.Marionette
-import GHC.IsList (IsList (Item))
-import Test.Dramaturge.Config (Config)
-import Prelude
+import Effectful.Process.Typed
+import Effectful.Timeout (Timeout, runTimeout, timeout)
+import GHC.Stack (HasCallStack, withFrozenCallStack)
+import Test.Dramaturge.Config
+import Test.Dramaturge.Firefox (withFirefox)
+import Test.Dramaturge.Log
+import Prelude hiding (writeFile)
 
-data Dramaturge :: Effect
+runDramaturge
+    :: (IOE :> es)
+    => Config
+    -> Eff
+        ( FileSystem
+            ': Concurrent
+            ': Timeout
+            ': TypedProcess
+            ': Log
+            ': Marionette
+            ': es
+        )
+        a
+    -> Eff es a
+runDramaturge Config{..} =
+    runFileSystem
+        . runConcurrent
+        . runTimeout
+        . runTypedProcess
+        . withFirefox firefox
+        . runMarionette
+        . runLog logLevel
+        . inject
 
-type role Dramaturge phantom phantom
+newtype TimeoutException = TimeoutException String
+    deriving anyclass (Exception)
 
-type instance DispatchOf Dramaturge = 'Static 'WithSideEffects
+instance Show TimeoutException where
+    show (TimeoutException s) = "Timed out waiting for " <> s
 
-newtype instance StaticRep Dramaturge = Dramaturge Config
+withTimeout :: (Timeout :> es) => String -> Int -> Eff es a -> Eff es a
+withTimeout (TimeoutException -> e) µs = maybe (throwM e) pure <=< timeout µs
 
--- runDramaturge :: (IOE :> es) => DramaturgeConfig -> Eff (Dramaturge ': es) a -> Eff es a
--- runDramaturge conf effect = do
---     -- session <- liftIO $ Dramaturge.runSession conf Dramaturge.getSession
---     evalStaticRep (Dramaturge conf) effect
---
--- scrollIntoView :: (Dramaturge :> es) => Element -> Eff es ()
--- scrollIntoView = executeScript "arguments[0].scrollIntoView({behavior: 'instant', block: 'nearest'})" . Identity . toJSON
---
--- isVisible :: (Dramaturge :> es) => Element -> Eff es Bool
--- isVisible = executeScript "arguments[0].checkVisibility()" . Identity . toJSON
---
--- isDisabled :: (Dramaturge :> es) => Element -> Eff es Bool
--- isDisabled = fmap isJust . Marionette.getElementAttribute "disabled"
---
--- isEnabled :: (Dramaturge :> es) => Element -> Eff es Bool
--- isEnabled = fmap not . isDisabled
---
--- findAll :: (Dramaturge :> es, IsList list, Item list ~ Element) => Selector -> Eff es list
--- findAll = Marionette.findElements
---
--- findOne :: (Dramaturge :> es) => Selector -> Eff es Element
--- findOne =
---     findAll >=> \case
---         [e] -> pure e
---         _ -> throwM . AssertionFailed $ "findOne: expected to find one element"
---
--- click :: (Dramaturge :> es) => Element -> Eff es ()
--- click e = do
---     Marionette.elementClick e
+waitFor
+    :: forall a es
+     . ( HasCallStack
+       , Concurrent :> es
+       , Timeout :> es
+       , Log :> es
+       )
+    => String
+    -> Eff es a
+    -> Eff es a
+waitFor what action =
+    withTimeout what maxDuration . untilJustM $ do
+        (Just <$> action) `catch` \(e :: SomeException) -> do
+            logTrace_ . fromString . displayException @SomeException $ e
+            threadDelay stepDuration
+            pure Nothing
+  where
+    maxDuration, stepDuration :: Int
+    maxDuration = 5_000_000
+    stepDuration = 10_000
+
+waitForElementThat
+    :: ( HasCallStack
+       , Concurrent :> es
+       , Timeout :> es
+       , Log :> es
+       , Marionette :> es
+       )
+    => (Element -> Eff es ())
+    -> Selector
+    -> Eff es Element
+waitForElementThat test selector = withFrozenCallStack $ waitFor what do
+    e <- findElement selector
+    test e
+    pure e
+  where
+    what = "element (" <> show selector <> ")"
+
+waitForElement
+    :: ( HasCallStack
+       , Concurrent :> es
+       , Timeout :> es
+       , Log :> es
+       , Marionette :> es
+       )
+    => Selector
+    -> Eff es Element
+waitForElement = waitForElementThat (const $ pure ())
+
+instance (Exception e) => Exception [e]
+
+tryThese
+    :: (HasCallStack, Concurrent :> es)
+    => Eff es a
+    -> Eff es b
+    -> Eff es (These a b)
+tryThese a b =
+    concurrently (try @SomeException a) (try b) >>= \case
+        (Right a, Left _) -> pure (This a)
+        (Left _, Right b) -> pure (That b)
+        (Right a, Right b) -> pure (These a b)
+        (Left a, Left b) -> throwM [a, b]
+
+findTheseElems
+    :: ( HasCallStack
+       , Concurrent :> es
+       , Log :> es
+       , Marionette :> es
+       )
+    => Selector
+    -> Selector
+    -> Eff es (These Element Element)
+findTheseElems s1 s2 = do
+    logTraceShow_ (s1, s2)
+    withFrozenCallStack $ tryThese (findElement s1) (findElement s2)
+
+getSource :: (HasCallStack, Marionette :> es) => Eff es Text
+getSource = executeScript "return new XMLSerializer().serializeToString(document);" []
+
+saveSource
+    :: (HasCallStack, FileSystem :> es, Marionette :> es)
+    => FilePath
+    -> Eff es ()
+saveSource f = writeFile f . Text.encodeUtf8 =<< getSource
+
+scrollIntoView
+    :: (HasCallStack, Marionette :> es, Log :> es)
+    => Element
+    -> Eff es ()
+scrollIntoView e = do
+    logTraceShow_ e
+    executeScript
+        "arguments[0].scrollIntoView({behavior: 'instant', block: 'nearest'})"
+        [toJSON e]
+
+click :: (HasCallStack, Marionette :> es, Log :> es) => Element -> Eff es ()
+click e = do
+    scrollIntoView e
+    logTraceShow_ e
+    elementClick e
